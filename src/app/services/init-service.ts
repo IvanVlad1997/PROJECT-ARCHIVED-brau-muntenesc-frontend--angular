a8d4a185dc51c2cad76466146ac2bdc624f1a636
IvanVlad1997 <ivanvlad97@yahoo.ro>
2021-02-02 13:53:24 +0200

    init

--- src/app/services/init-service.ts
+++ src/app/services/init-service.ts
@@ -1,14 +1,26 @@
 import {Injectable} from '@angular/core';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {AuthService} from '../../../projects/auth/src/lib/services/auth';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Injectable({providedIn: 'root'})
 export class InitService {
   constructor(private angularFirebaseAuth: AngularFireAuth,
-              private authService: AuthService) {
+              private authService: AuthService,
+              private http: HttpClient) {
   }
 
   async start(): Promise<void>{
+    await this.http.get(`${environment.appApi}/cool`).toPromise().then(
+      async (c) =>  {
+        console.log('backend on');
+        await this.loadCurrentUser()
+      }
+    )
+  }
+
+  async loadCurrentUser(): Promise<void> {
     this.angularFirebaseAuth.authState.subscribe(async (user) => {
       // console.log('Se incerca conectarea la user-ul curent');
       if (user){
